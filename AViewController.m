b381b4521804e9cd1f78e27663daaf5aa0ca4012
Puma <zzy_9613@sohu.com>
2015-06-10 09:55:21 +0000

    添加了新的viewController对象

---
+++ AViewController.m
@@ -0,0 +1,37 @@
+//
+//  AViewController.m
+//  BSMusicChose
+//
+//  Created by Puma on 15/6/10.
+//  Copyright (c) 2015年 邹志勇. All rights reserved.
+//
+
+#import "AViewController.h"
+
+@interface AViewController ()
+
+@end
+
+@implementation AViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
